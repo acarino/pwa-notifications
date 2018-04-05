33859061fdd79cc53645897c0ed07241b7a84ebd
Alan Carino <alan.carino@thomsonreuters.com>
2018-04-05 10:03:59 -0400

    updated git ignore and adding listener external js file

---
+++ src/listener.js
@@ -0,0 +1,8 @@
+//listener.js
+
+window.addEventListener('click', function(e) {
+  //var notification = e.notification;
+  //var primaryKey = notification.data.primaryKey;
+
+  console.log('in my event listener: ' + e);
+});
